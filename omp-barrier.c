9b8875a4232a21fb2e99edd40671bdf7d4348986
TrashRatInTrash <Rueslin.Rana@outlook.com>
2025-03-17 08:43:00 +0200

    added time measurement

--- omp-barrier.c
+++ omp-barrier.c
@@ -1,9 +1,12 @@
 #include <omp.h>
 #include <stdio.h>
-#define MAX_NUM 100
+#include <stdlib.h>
+#include <time.h>
 
-void print_Array(int *arr, int size){
-  for(int i = 0; i < size;i++){
+#define MAX_NUM 1000000
+
+void print_Array(int *arr, int size) {
+  for (int i = 0; i < size; i++) {
     printf("%d\n", arr[i]);
   }
 }
@@ -14,6 +17,9 @@ int main() {
   int b[MAX_NUM];
   int n = MAX_NUM;
   int i;
+
+  clock_t t = clock();
+
 #pragma omp parallel shared(n, b, a) private(i)
   {
 #pragma omp for
@@ -27,10 +33,14 @@ int main() {
     }
   }
 
-  printf("A\n");
-  print_Array(a, n);
-  printf("B\n");
-  print_Array(b, n);
-  
-  
+  t = clock() - t;
+  double time_taken = ((double)t) / CLOCKS_PER_SEC;
+  printf("CLOCKS_PER_SEC: %f\n", (double)CLOCKS_PER_SEC);
+  printf("%f seconds\n", time_taken);
+
+  // Uncomment the following lines if you want to print the arrays (not
+  // recommended for large n) printf("A\n"); print_Array(a, n); printf("B\n");
+  // print_Array(b, n);
+
+  return 0;
 }
