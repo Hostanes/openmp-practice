a5d86983a6ae892f25ceb9e40c584aac6ab32956
TrashRatInTrash <Rueslin.Rana@outlook.com>
2025-03-17 08:53:19 +0200

    both clock and timeval

--- omp-barrier.c
+++ omp-barrier.c
@@ -1,6 +1,7 @@
 #include <omp.h>
 #include <stdio.h>
-#include <sys/time.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define MAX_NUM 1000000
 
@@ -17,16 +18,7 @@ int main() {
   int n = MAX_NUM;
   int i;
 
-  static double t_begin = 0.0;
-  static int sec = -1;
-  struct timeval val;
-  if (sec < 0) {
-    sec = val.tv_sec;
-  }
-  t_begin = (val.tv_sec - sec) + (1.0e-6 * val.tv_usec);
-
-  // struct timeval tvalBefore, tvalAfter;
-  // gettimeofday(&tvalBefore, NULL);
+  clock_t t = clock();
 
 #pragma omp parallel shared(n, b, a) private(i)
   {
@@ -41,22 +33,14 @@ int main() {
     }
   }
 
-  static double t_end = 0.0;
-  sec = -1;
-  if (sec < 0) {
-    sec = val.tv_sec;
-  }
-  t_end = (val.tv_sec - sec) + (1.0e-6 * val.tv_usec);
+  t = clock() - t;
+  double time_taken = ((double)t) / CLOCKS_PER_SEC;
+  printf("CLOCKS_PER_SEC: %f\n", (double)CLOCKS_PER_SEC);
+  printf("%f seconds\n", time_taken);
 
-  // gettimeofday(&tvalBefore, NULL);
-
-  // printf("A\n");
-  // print_Array(a, n);
-  // printf("B\n");
+  // Uncomment the following lines if you want to print the arrays (not
+  // recommended for large n) printf("A\n"); print_Array(a, n); printf("B\n");
   // print_Array(b, n);
 
-  printf("Time taken %0.4f", t_end - t_begin);
-
-  // printf("Time in seconds: %0.3f seconds\n",
-  //        (float)(tvalAfter.tv_sec - tvalBefore.tv_sec));
+  return 0;
 }
